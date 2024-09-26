4723eab496e54566d8282085b3636e44d3fcb959
Mantis417 <mook.azure@gmail.com>
2024-09-26 07:38:47 +0200

    structure fix

--- docker-init.sh
+++ docker-init.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Add Docker's official GPG key:
+
 sudo apt-get update
 sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add the repository to Apt sources:
+
 sudo echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
@@ -15,14 +15,14 @@ sudo echo \
 sudo apt-get update
 
 
-# Installing Docker:
+
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
-# Create directory for compose.yml
+
 sudo mkdir -p myapp
 cd /myapp
 
-# Create docker-compose.yml dynamically
+
 sudo tee /myapp/docker-compose.yml > /dev/null <<EOL
 version: '3'
 services:
